c772166edb4a099ac1ae18a66f7039babdee0af4
Fabrizio <whoet.wo@gmail.com>
2020-11-17 15:27:22 +0100

    Esercizio completato

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const app = new Vue({
+   el: '.my_container',
+   data: {
+      myArray: [
+         'img/image1.jpg',
+         'img/image2.jpg',
+         'img/image3.jpg',
+         'img/image4.jpg'
+      ],
+      myIndex: 0,
+   },
+   methods:{
+      prevClick: function(){
+         if(this.myIndex != 0){
+            this.myIndex--
+         }else{
+            this.myIndex = this.myArray.length - 1
+         }
+      },
+      forwClick: function(){
+         if(this.myIndex != this.myArray.length - 1){
+            this.myIndex++
+         }else{
+            this.myIndex = 0
+         }
+      }
+   }
+})
